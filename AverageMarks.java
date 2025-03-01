9c758542e3a3c6cb981aaddec8e66d3256869c24
Yagyata <yagyatasharmaa@gmail.com>
2025-03-01 10:15:45 +0530

    [Yagyata] ADD:Calculating the average marks in PCM

---
+++ AverageMarks.java
@@ -0,0 +1,9 @@
+class AverageMarks{
+	public static void main(String[] args){
+		int mathsMarks = 94;
+		int chemistryMarks = 96;
+		int physicsMarks = 95;
+		int Average = (mathsMarks + chemistryMarks + physicsMarks)/3; //average of three subjects will be S1+S2+S3/3;
+		System.out.println("Sam's average marks in PCM is " + Average);
+	}
+}
