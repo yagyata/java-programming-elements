c8a5706b1f551a7a81c5c5d7c7b98dc3424ee71b
Yagyata <yagyatasharmaa@gmail.com>
2025-03-01 10:11:55 +0530

    [Yagyata] ADD:Finding Harry's age

---
+++ HarryAge.java
@@ -0,0 +1,9 @@
+class HarryAge{
+	public static void main(String[] args){
+		int birthYear = 2000;
+		int currentYear =2024;
+		int presentAge = currentYear - birthYear;
+		System.out.println("Harry's age in 2024 is: "+ presentAge);
+	}
+}
+
